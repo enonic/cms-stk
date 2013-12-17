29cd19a1f7ed4976056c844b1a3e0db18f1d2087
Tor Løkken <tor.lokken@gmail.com>
2013-12-17 09:28:01 +0100

    Adjusted submenu css selector to reflect changes in navigation.xsl. Added aria-label to branch-button

--- _public/library-stk/js/stk.js
+++ _public/library-stk/js/stk.js
@@ -15,20 +15,19 @@ STK.navigation = {
     mobileMenu: function (tree) {
         // adds branch buttons for expanding/contracting submenu
         tree.find('li.parent').each(function () {
-            var branchButton = $(document.createElement('a')).addClass('branch-button').attr('href', '#');
-            $(this).prepend(branchButton);
-        });
-        // bind branch button click event
-        tree.find('.branch-button').on('click', function (e) {
-            $(this).nextAll('ul').each(function () {
-                if ($(this).is(':visible')) {
-                    $(this).parent().addClass('contracted').removeClass('expanded');
-                } else {
-                    $(this).parent().addClass('expanded').removeClass('contracted');
-                }
-                $(this).slideToggle();
+            var branchButton = $(document.createElement('button')).addClass('branch-button').attr('aria-label', 'Toggle submenu');
+            branchButton.on('click', function (e) {
+                $(this).nextAll('ol').each(function () {
+                    if ($(this).is(':visible')) {
+                        $(this).parent().addClass('contracted').removeClass('expanded');
+                    } else {
+                        $(this).parent().addClass('expanded').removeClass('contracted');
+                    }
+                    $(this).slideToggle();
+                });
+                e.preventDefault();
             });
-            e.preventDefault();
+            $(this).prepend(branchButton);
         });
     },
     toggleMenu: function (menu, trigger, options) {
@@ -53,58 +52,6 @@ STK.navigation = {
         });
     }
 };
-
-/*STK.pagination = {
-// add ajax (click triggered) pagination to a content list
-clickLoad: function (list, callback) {
-var indexCounter = 0;
-var count = parseInt(list.data('count'));
-var totalCount = parseInt(list.data('totalcount'));
-var windowUrl = list.data('windowurl');
-var showMoreText = (list.data('show-more-text') || 'Show #count more').replace('#count', count);
-var showingText = function() {
-return (list.data('showing-text') || 'Showing #count of #totalcount').replace('#totalcount', totalCount).replace('#count', count + indexCounter);
-}
-
-if (count && totalCount && windowUrl && count < totalCount) {
-var wait = false;
-var trigger = $('<div/>').addClass('pagination click-load');
-trigger.append($('<div/>').addClass('show-more').text(showMoreText));
-trigger.append($('<div/>').addClass('showing').text(showingText()));
-trigger.insertAfter(list);
-trigger.click(function () {
-if (!wait) {
-trigger.addClass('loading');
-wait = true;
-$.ajax({
-url: windowUrl.replace('REPLACEWITHINDEX', indexCounter += count),
-success: function(data) {
-var showingCount = Math.min(indexCounter + count, totalCount);
-if (showingCount === totalCount) {
-trigger.hide();
-}
-trigger.find('.showing').text(showingText());
-var listElements = $(document.createElement('div')).append(data).find(list.selector + ' > li');
-list.append(listElements);
-},
-error: function() {
-indexCounter -= count;
-},
-complete: function() {
-if (typeof(callback) == 'function') {
-callback();
-}
-}
-}).always( function() {
-trigger.removeClass('loading');
-wait = false;
-});
-}
-});
-}
-}
-};*/
-
 
 STK.pagination = {
     // add ajax (click triggered) pagination to a content list
